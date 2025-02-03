2f0b139ebe431712c7a437599be46efce85a6c25
prathapstd2@gmail.com <prathapstd2@gmail.com>
2025-02-04 02:15:27 +0530

    modify

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+// function toggleMenu() {
+//     const menu = document.querySelector(".menu-links");
+//     const icon = document.querySelector(".hamburger-icon");
+//     menu.classList.toggle("open");
+//     icon.classList.toggle("open");
+//   }
+// const handbracker = document.getElementsByClassName("handbracker")[0];
+// const navlinks = document.getElementsByClassName("navlinks")[0];
+// handbracker.addEventListener('click',()=>{
+//     navlinks.classList.toggle("active");
+// });
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+    const menu = document.querySelector(".navlinks");
+    const icon = document.querySelector(".hamburger");
+    menu.classList.toggle("active");
+    icon.classList.toggle("active");
   }
-  
