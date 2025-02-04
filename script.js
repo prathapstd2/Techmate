0f2f6a17b6e0c860990f1069ed0671afc7861c79
prathapstd2@gmail.com <prathapstd2@gmail.com>
2025-02-04 09:19:15 +0530

    yyy

--- script.js
+++ script.js
@@ -15,20 +15,3 @@ function toggleMenu() {
     menu.classList.toggle("active");
     icon.classList.toggle("active");
   }
-let lastScrollTop = 0; // Keeps track of the last scroll position
-const navbar = document.getElementById("desktop-nav"); // The navbar
-
-window.addEventListener("scroll", function() {
-  let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-
-  // If scrolling down
-  if (currentScroll > lastScrollTop) {
-    navbar.style.top = "-70px"; // Hide the navbar (adjust based on the height of your navbar)
-  } else {
-    // If scrolling up
-    navbar.style.top = "0"; // Show the navbar
-  }
-
-  // Update lastScrollTop to current scroll position
-  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
-});
